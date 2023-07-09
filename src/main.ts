2b996013b3a079ce085be364e7ae46ca89138a33
adv-jvaleriano <juan.valeriano@adviters.com>
2023-07-09 18:47:23 -0300

    Fix Initial commit

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+
+  const PORT: number = 3000;
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  await app.listen(
+    PORT, 
+    () => console.info(`app listening on port: ${PORT}`)
+    );
 }
 bootstrap();
